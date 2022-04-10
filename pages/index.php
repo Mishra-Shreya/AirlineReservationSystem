fdab18fb84d3de3585e10248d81f7d5e29765fd8
Mishra-Shreya <6901shreyamishra2001@gmail.com>
2022-04-10 14:04:41 +0530

    changes in assets/ and pages/ dir

---
+++ pages/index.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+<title>RIS Traveler</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="../assets/css/plugins.css">
+<link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+</head>
+<body>
+<div id="fh5co-wrapper">
+  <div id="fh5co-page">
+    <header id="fh5co-header-section" class="sticky-banner">
+      <div class="container">
+        <div class="nav-header"><a href="javascript:void(0)" class="js-fh5co-nav-toggle fh5co-nav-toggle dark"><i></i></a>
+          <h1 id="fh5co-logo"><a href="index.php">.Traveler.</a></h1>
+          <nav id="fh5co-menu-wrap" role="navigation">
+            <ul class="sf-menu" id="fh5co-primary-menu">
+              <li class="active"><a href="index.php">Airline Reservation system</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+    <div class="fh5co-hero">
+      <div class="fh5co-overlay"></div>
+      <div class="fh5co-cover" data-stellar-background-ratio="0.5" style="background-image:url(../assets/images/cover_bg_3.jpg);">
+        <div class="desc">
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-5 col-md-5">
+                <div class="tabulation animate-box">
+                  <ul class="nav nav-tabs">
+                    <li class="active"><a href="#" >Flights</a></li>
+                  </ul>
+                  <form action="#" method="post">
+                    <div class="tab-content">
+                      <div role="tabpanel" class="tab-pane active" id="flights">
+                        <div class="row">
+                          <div class="col-xxs-12 col-xs-6 mt">
+                            <div class="input-field">
+                              <label>From:</label>
+                              <input type="text" class="form-control" placeholder="Los Angeles, USA">
+                            </div>
+                          </div>
+                          <div class="col-xxs-12 col-xs-6 mt">
+                            <div class="input-field">
+                              <label>To:</label>
+                              <input type="text" class="form-control" placeholder="Tokyo, Japan">
+                            </div>
+                          </div>
+                          <div class="col-sm-12 mt alternate">
+                            
+                            <div class="input-field">
+                              <label>Date:</label>
+                              <input type="date" class="form-control" id="datepicker" placeholder="mm/dd/yyyy">
+                            </div>
+                          </div>
+                          <!-- <div class="col-xxs-12 col-xs-6 mt alternate">
+                            <div class="input-field">
+                              <label>Check Out:</label>
+                              <input type="text" class="form-control" placeholder="mm/dd/yyyy">
+                            </div>
+                          </div> -->
+                          <div class="col-sm-12 mt">
+                            <div class="form-group">
+                              <label for="class">Class:</label>
+                              <select class="form-control" id="class">
+                                <option>Economy</option>
+                                <option>Business</option>
+                              </select>
+                            </div>
+                          </div>
+                          <div class="col-xxs-12 col-xs-6 mt">
+                            <div class="form-group">
+                              <label for="adults">Adults:</label>
+                              <select class="form-control" id="adults">
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                              </select>
+                            </div>
+                          </div>
+                          <div class="col-xxs-12 col-xs-6 mt">
+                            <div class="form-group">
+                              <label for="adults">Children:</label>
+                              <select class="form-control" id="adults">
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                              </select>
+                            </div>
+                          </div>
+                          <div class="col-xs-12">
+                            <input type="submit" class="btn btn-primary btn-block" value="Search Flight">
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+              <div class="desc2 animate-box">
+                <div class="col-sm-7 col-md-7">
+                  <p>Lorem ipsum dolor sit amet</p>
+                  <h2>Search your flights here</h2>
+                  <h3>Fly to Anywhere</h3>
+                  <span class="price">₹1999</span></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div id="fh5co-tours" class="fh5co-section-gray">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-md-offset-2 text-center heading-section animate-box">
+            <h3>Book A Flight</h3>
+            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+          </div>
+        </div>
+        <div class="here">
+          
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.7.1/js/bootstrap-datepicker.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
